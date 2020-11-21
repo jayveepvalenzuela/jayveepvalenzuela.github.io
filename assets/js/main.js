63e471c44faee2db33aeeac22d7e62a7696063dc
Jayvee Valenzuela <jayveevalenzuela@outlook.com>
2020-11-21 08:29:12 +0800

    Custom cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,20 +12,70 @@ selectAllElements('.lazy').forEach((el) => {
     el.src = el.dataset.src;
 });
 
+// theme toggle
+const btnThemeToggle = body.appendChild(createElement('button'));
+
+btnThemeToggle.classList.add('btn-theme-toggle', 'lar', 'cursor-animate');
+
+btnThemeToggle.addEventListener('click', (ev) => {
+    body.setAttribute('data-theme', Number(ev.target.classList.toggle('light')));
+});
+
+// cursor look and behavior
+const cursorPointer = body.appendChild(createElement('div'));
+const cursorRing = cursorPointer.appendChild(createElement('div'));
+const cursor = {
+    x: 0,
+    y: 0,
+    ringX: 0,
+    ringY: 0
+};
+
+cursorPointer.classList.add('cursor-pointer');
+cursorRing.classList.add('cursor-ring');
+
+body.addEventListener('mousemove', (ev) => {
+    cursor.x = ev.clientX;
+    cursor.y = ev.clientY;
+});
+
+gsap.to({}, 0.010, {
+    repeat: -1,
+    onRepeat: () => {
+        cursor.ringX += (cursor.x - cursor.ringX) / 6;
+        cursor.ringY += (cursor.y - cursor.ringY) / 6;
+
+        gsap.set(cursorRing, {
+            css: {
+                left: cursor.ringX - 15,
+                top: cursor.ringY - 15
+            }
+        });
+
+        gsap.set(cursorPointer, {
+            css: {
+                left: cursor.x,
+                top: cursor.y
+            }
+        });
+    }
+});
+
+selectAllElements('.cursor-animate').forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        cursorPointer.classList.add('hover');
+    });
+
+    el.addEventListener('mouseleave', () => {
+        cursorPointer.classList.remove('hover');
+    });
+});
+
 // navbar toggle
 const navbarMenu = selectElement('.navbar__menu');
 
 selectElement('.navbar__toggle').addEventListener('click', () => {
     navbarMenu.classList.toggle('visible');
-});
-
-// theme toggle
-const btnThemeToggle = body.appendChild(createElement('button'));
-
-btnThemeToggle.classList.add('btn-theme-toggle', 'lar');
-
-btnThemeToggle.addEventListener('click', (ev) => {
-    body.setAttribute('data-theme', Number(ev.target.classList.toggle('light')));
 });
 
 window.addEventListener('load', () => {
